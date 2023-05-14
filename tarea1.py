11edcf5bb398a94b1d267b746d222de25577f1b7
Jhonatan <133496183+Jhonatan9923@users.noreply.github.com>
2023-05-13 21:23:37 -0500

    Add files via upload

---
+++ tarea1.py
@@ -0,0 +1,10 @@
+weather = input("What is the weather today? ")
+
+if weather == "sunny":
+    print("It's a beautiful day outside!")
+elif weather == "rainy":
+    print("Bring an umbrella!")
+elif weather == "snowy":
+    print("Bundle up!")
+else:
+    print("I don't know what to tell you.")
